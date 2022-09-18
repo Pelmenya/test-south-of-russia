4e79e9e3ee97858a4f83462d6c0c0053a2c8dd80
Dmitry Lyapin <lyapindm@yandex.ru>
2022-09-18 15:06:43 +0300

    Переключил на NewsAPI

--- src/utils/constants/api-routes.ts
+++ src/utils/constants/api-routes.ts
@@ -1,6 +1,6 @@
-// const serverUrlNews = 'https://nomoreparties.co/news/v2/everything';
+const serverUrlNews = 'https://nomoreparties.co/news/v2/everything';
 // const serverUrlNews = 'https://newsapi.org/v2/everything';
-/*
+
 const apiKeyNews = '45a6a26cce284a36ba341837300b2c6b';
 const pageSizeNews = 100;
 const numberOfDays = 30;
@@ -11,5 +11,5 @@ const to = date.toISOString().slice(0, 19);
 export const BASE_URL = `
     ${serverUrlNews}?q=${keyWord}&pageSize=${pageSizeNews}&from=${from}&to=${to}&apiKey=${apiKeyNews}
 `;
- */
-export const BASE_URL = 'https://saurav.tech/NewsAPI/everything/cnn.json';
+
+// export const BASE_URL = 'https://saurav.tech/NewsAPI/everything/cnn.json';
